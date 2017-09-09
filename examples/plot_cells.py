92ed72822035d0e73e74c638e30a3b7b54de075c
Nicholas Sauter <nksauter@lbl.gov>
2017-09-09 15:39:35 -0700

    Basic examples: create list of unit cells, plot it, cluster it with Zeldin program.

---
+++ examples/plot_cells.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import sys
+data = open (sys.argv[1],"r").readlines()
+a_cell = []
+c_cell = []
+for line in data:
+  tokens = line.strip().split()
+  a = float(tokens[0])
+  c = float(tokens[2])
+  a_cell.append(a)
+  c_cell.append(c)
+
+from matplotlib import pyplot as plt
+plt.plot(a_cell,c_cell,"r,")
+plt.axes().set_aspect("equal")
+plt.show()
---
+++ examples/summarize_cells.py
@@ -0,0 +1,20 @@
+from __future__ import division
+import sys
+
+"""Simply convert integration pickles to a text file with unit cell and
+space group symbol"""
+from xfel.command_line.print_pickle import generate_data_from_streams
+if __name__=="__main__":
+  args = sys.argv[1:]
+
+  for data in generate_data_from_streams(args, verbose=False):
+
+    obs = data["observations"][0]
+
+    unit_cell = obs.unit_cell()
+    params = unit_cell.parameters()
+    for p in params: print p,
+
+
+    space_group = obs.space_group_info().type().lookup_symbol()
+    print "".join(space_group.split())
