a23c52874809f8a69e4f1742689275e46a59a753
Nicholas Sauter <nksauter@lbl.gov>
2017-10-08 19:17:05 -0700

    Refactor, fast plots.

--- examples/mbh/mbh.py
+++ examples/mbh/mbh.py
@@ -1,64 +1,33 @@
 from __future__ import division
 from cctbx.array_family import flex
 from libtbx.development.timers import Profiler
-
+from libtbx import group_args
 """
 Initial implementation of clustering by Rodriguez 2014 algorithm
 """
-def run_detail(show_plot, save_plot):
-    P = Profiler("0. Read data")
-    import pickle
-    DP = pickle.load(open("mbhresults.pickle","rb"))
-    coord_x = DP.get("coord_x")#[:500]
-    coord_y = DP.get("coord_y")#[:500]
-
-    print("There are %d points"%(len(coord_x)))
-    if show_plot or save_plot:
-      import matplotlib
-      if not show_plot:
-        # http://matplotlib.org/faq/howto_faq.html#generate-images-without-having-a-window-appear
-        matplotlib.use('Agg') # use a non-interactive backend
-      from matplotlib import pyplot as plt
-      #plt.plot(coord_x.select(selection),coord_y.select(selection),"r.", markersize=2.)
-      #plt.plot(coord_x.select(~selection),coord_y.select(~selection),"k.", markersize=3.)
-      plt.plot(coord_x,coord_y,"k.", markersize=3.)
-      #plt.plot(coord_x.select(selection_1F400),coord_y.select(selection_1F400),"r.", markersize=3.)
-      #plt.plot(coord_x.select(selection_2F400),coord_y.select(selection_2F400),"k.", markersize=3.)
-      plt.axes().set_aspect("equal")
-      if save_plot:
-        plt.savefig(plot_name,
-                    size_inches=(10,10),
-                    dpi=300,
-                    bbox_inches='tight')
-      if show_plot:
-        plt.show()
-
-    print "Now constructing a Dij matrix."
-    P = Profiler("1. compute Dij matrix")
-    NN = len(coord_x)
-    embedded_vec2 = flex.vec2_double(coord_x,coord_y)
-    Dij = embedded_vec2.distance_matrix(embedded_vec2)
-
+class clustering_manager(group_args):
+  def __init__(self, **kwargs):
+    group_args.__init__(self, **kwargs)
+    # require Dij, d_c
     P = Profiler("2. calculate rho density")
     print "finished Dij, now calculating rho_i, the density"
-    max_Dij = flex.max(Dij)
-    d_c = 0.04 # a guess, for now
     from xfel.clustering import Rodriguez_Laio_clustering_2014
-    R = Rodriguez_Laio_clustering_2014(distance_matrix = Dij, d_c = d_c)
-    rho = R.get_rho()
+    R = Rodriguez_Laio_clustering_2014(distance_matrix = self.Dij, d_c = self.d_c)
+    self.rho = rho = R.get_rho()
     ave_rho = flex.mean(rho.as_double())
+    NN = self.Dij.focus()[0]
     print "The average rho_i is %5.2f, or %4.1f%%"%(ave_rho, 100*ave_rho/NN)
     i_max = flex.max_index(rho)
 
     P = Profiler("3.transition")
     print "the index with the highest density is %d"%(i_max)
-    delta_i_max = flex.max(flex.double([Dij[i_max,j] for j in xrange(NN)]))
+    delta_i_max = flex.max(flex.double([self.Dij[i_max,j] for j in xrange(NN)]))
     print "delta_i_max",delta_i_max
     rho_order = flex.sort_permutation(rho,reverse=True)
     rho_order_list = list(rho_order)
 
     P = Profiler("4. delta")
-    delta = R.get_delta(rho_order=rho_order, delta_i_max=delta_i_max)
+    self.delta = delta = R.get_delta(rho_order=rho_order, delta_i_max=delta_i_max)
 
     P = Profiler("5. find cluster maxima")
     #---- Now hunting for clusters
@@ -83,41 +52,18 @@ def run_detail(show_plot, save_plot):
     for x in xrange(NN):
       if cluster_id[x]>=0:
         print "XC",x,cluster_id[x],rho[x],delta[x]
-
-    from matplotlib import pyplot as plt
-    #plt.plot(rho,delta,"r.", markersize=3.)
-    #for x in xrange(NN):
-    #  if cluster_id[x]>=0:
-    #    plt.plot([rho[x]],[delta[x]],"ro")
-    #plt.show()
+    self.cluster_id_maxima = cluster_id.deep_copy()
 
     P = Profiler("6. assign all points")
     R.cluster_assignment(rho_order,cluster_id)
 
-    #for idx in xrange(NN):
-    #
-    #  plt.plot([coord_x[idx]],[coord_y[idx]],"%so"%(
-    #    {-1:'k',0:'b',1:'r',2:'g'}[cluster_id[idx]]
-    #))
-    #plt.show()
+    self.cluster_id_full = cluster_id.deep_copy()
 
     # assign the halos
     P = Profiler("7. assign halos")
     halo = flex.bool(NN,False)
-
-    border = flex.bool(NN,False)
-    for i in xrange(0,NN):
-      for j in xrange(i+1,NN):
-        if Dij[(i,j)] < d_c: # find points being within d_c of those in another cluster
-          if cluster_id[i] != cluster_id[j]:
-            border[i]=True; border[j]=True
-    cpp_border = R.get_border( cluster_id = cluster_id )
-    for ix in xrange(NN):
-      assert cpp_border[ix] == border[ix]
-
     border = R.get_border( cluster_id = cluster_id )
 
-    P = Profiler("8. part")
     for ic in range(n_cluster): #loop thru all border regions; find highest density
       print "cluster",ic, "in border",border.count(True)
       this_border = (cluster_id == ic) & (border==True)
@@ -132,21 +78,78 @@ def run_detail(show_plot, save_plot):
         too_sparse = core_selection & (rho.as_double() < highest_density/10.) # another heuristic
         if too_sparse.count(True)>0:
           cluster_id.set_selected(too_sparse,-1)
-    exit()
-    for idx in xrange(NN):
-      if cluster_id[idx]==-1:
-        plt.plot([coord_x[idx]],[coord_y[idx]],"%s."%(
-        {-1:'k',0:'b',1:'r',2:'g'}[cluster_id[idx]]))
-      else:
-        plt.plot([coord_x[idx]],[coord_y[idx]],"%so"%(
-        {-1:'k',0:'b',1:'r',2:'g'}[cluster_id[idx]]))
+    self.cluster_id_final = cluster_id.deep_copy()
+    print "%d in the excluded halo"%((cluster_id==-1).count(True))
 
-    plt.show()
+def run_detail(show_plot, save_plot):
+    P = Profiler("0. Read data")
+    import pickle
+    DP = pickle.load(open("mbhresults.pickle","rb"))
+    coord_x = DP.get("coord_x")
+    coord_y = DP.get("coord_y")
+    del P
 
+    print("There are %d points"%(len(coord_x)))
+    if show_plot or save_plot:
+      import matplotlib
+      if not show_plot:
+        # http://matplotlib.org/faq/howto_faq.html#generate-images-without-having-a-window-appear
+        matplotlib.use('Agg') # use a non-interactive backend
+      from matplotlib import pyplot as plt
+      plt.plot(coord_x,coord_y,"k.", markersize=3.)
+      plt.axes().set_aspect("equal")
+      if save_plot:
+        plt.savefig(plot_name,
+                    size_inches=(10,10),
+                    dpi=300,
+                    bbox_inches='tight')
+      if show_plot:
+        plt.show()
+
+    print "Now constructing a Dij matrix."
+    P = Profiler("1. compute Dij matrix")
+    NN = len(coord_x)
+    embedded_vec2 = flex.vec2_double(coord_x,coord_y)
+    Dij = embedded_vec2.distance_matrix(embedded_vec2)
+    del P
+
+    d_c = 0.04 # the distance cutoff, such that average item neighbors 1-2% of all items
+    CM = clustering_manager(Dij=Dij, d_c=d_c)
+
+    appcolors = ['b', 'r', '#ff7f0e', '#2ca02c',
+              '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
+              '#bcbd22', '#17becf']
+    if show_plot:
+      #Decision graph
+      from matplotlib import pyplot as plt
+      plt.plot(CM.rho,CM.delta,"r.", markersize=3.)
+      for x in xrange(NN):
+        if CM.cluster_id_maxima[x]>=0:
+          plt.plot([CM.rho[x]],[CM.delta[x]],"ro")
+      plt.show()
+
+      #No-halo plot
+      from matplotlib import pyplot as plt
+      colors = [appcolors[i%10] for i in CM.cluster_id_full]
+
+      plt.scatter(coord_x,coord_y,marker='o',color=colors,
+          linewidths=0.4, edgecolor='k')
+      plt.axes().set_aspect("equal")
+      plt.show()
+
+      #Final plot
+      halo = (CM.cluster_id_final==-1)
+      core = ~halo
+      plt.plot(coord_x.select(halo),coord_y.select(halo),"k.")
+      colors = [appcolors[i%10] for i in CM.cluster_id_final.select(core)]
+      plt.scatter(coord_x.select(core),coord_y.select(core),marker="o",
+          color=colors,linewidths=0.4, edgecolor='k')
+      plt.axes().set_aspect("equal")
+      plt.show()
 
 if __name__=="__main__":
 
-  run_detail(show_plot=False, save_plot=False)
+  run_detail(show_plot=True, save_plot=False)
 """ Benchmark, 6672 MBH lattices
                    Python/Flex arrays;C++ Dij;C++ rho;C++delt;C clust;C bordr;
          0. Read data: CPU,    0.000s;  0.00s;  0.01s;  0.00s;  0.00s;  0.00s;
